24f86874e15d1982ccd203d2772ec57c3c36cb1e
Vui Nguyen <vui_engineer@yahoo.com>
2022-01-30 16:15:10 -0700

    Initial Commit

---
+++ TipperSwiftUI/TipperSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipperSwiftUIApp.swift
+//  TipperSwiftUI
+//
+//  Created by Vui Nguyen on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct TipperSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
