67ef5cb02164ef0978fe9594b4c937090f68a094
Vui Nguyen <vui_engineer@yahoo.com>
2022-02-01 10:29:08 -0700

    extract common text styling into extension

--- TipperSwiftUI/ContentView.swift
+++ TipperSwiftUI/ContentView.swift
@@ -20,10 +20,7 @@ struct ContentView: View {
     var body: some View {
         VStack(alignment: .leading) {
             Text("Bill Amount")
-                .font(.headline)
-                .fontWeight(.semibold)
-                .foregroundColor(Color(red: 0.19607843137254902, green: 0.22745098039215686, blue: 0.33725490196078434))
-                .padding()
+                .primaryStyle()
             TextField("$153.01", value: $billAmount, formatter: formatter)
                 .padding()
                 .textFieldStyle(RoundedBorderTextFieldStyle())
@@ -31,10 +28,7 @@ struct ContentView: View {
                 .foregroundColor(Color(red: 0.2627450980392157, green: 0.4588235294117647, blue: 0.8627450980392157))
                 .font(.title2)
             Text("Tip Percent")
-                .font(.headline)
-                .fontWeight(.semibold)
-                .padding()
-            
+                .primaryStyle()
         }
     }
 }
