2bacdafb34f46a013d5456205862855152a4bce4
Vui Nguyen <vui_engineer@yahoo.com>
2022-02-04 15:14:44 -0700

    refactor into smaller views

--- TipperSwiftUI/ContentView.swift
+++ TipperSwiftUI/ContentView.swift
@@ -37,34 +37,14 @@ struct ContentView: View {
             Spacer()
                 .frame(height: 30)
             VStack {
-                HStack(alignment: .center) {
-                    Text("Tip Amount")
-                        .primaryStyle()
-                    Spacer(minLength: 100)
-                    Text(viewModel.getTipAmountStringFormatted(tipPercent: selectedTipPercentage, billAmount: billAmount))
-                        .font(.subheadline)
-                        .fontWeight(.regular)
-                    // Color in hex #323A56
-                        .foregroundColor(Color(red: 0.19607843137254902, green: 0.22745098039215686, blue: 0.33725490196078434))
-                    Spacer()
-                        .frame(width: 20)
-                }
+
+                CalculatedAmountView(amountLabel: "Tip Amount", amount:  viewModel.getTipAmountStringFormatted(tipPercent: selectedTipPercentage, billAmount: billAmount))
 
                 Divider()
                     .padding(.horizontal)
 
-                HStack(alignment: .center) {
-                    Text("Bill Total")
-                        .primaryStyle()
-                    Spacer(minLength: 100)
-                    Text(viewModel.getBillTotalStringFormatted(tipPercent: selectedTipPercentage, billAmount: billAmount))
-                        .font(.title2)
-                        .fontWeight(.bold)
-                    // Color in hex #323A56
-                        .foregroundColor(Color(red: 0.19607843137254902, green: 0.22745098039215686, blue: 0.33725490196078434))
-                    Spacer()
-                        .frame(width: 20)
-                }
+                // for bill total, make font = .title2, and fontWeight = .bold
+                CalculatedAmountView(amountLabel: "Bill Total", amount:  viewModel.getBillTotalStringFormatted(tipPercent: selectedTipPercentage, billAmount: billAmount))
             }
             // Color in hex #E4ECFA
             .background(Color(red: 0.8941176470588236, green: 0.9254901960784314, blue: 0.9803921568627451))
@@ -77,5 +57,46 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CalculatedAmountView: View {
+    let amountLabel: String
+    let amount: String
+
+    var body: some View {
+        HStack(alignment: .center) {
+            Text(amountLabel)
+                .primaryStyle()
+            Spacer(minLength: 100)
+            Text(amount)
+                .font(.subheadline)
+                .fontWeight(.regular)
+            // Color in hex #323A56
+                .foregroundColor(Color(red: 0.19607843137254902, green: 0.22745098039215686, blue: 0.33725490196078434))
+            Spacer()
+                .frame(width: 20)
+        }
+    }
+}
+
+struct TipAmountView: View {
+    @Binding var selectedTipPercentage: TipPercent
+    @Binding var billAmount: Float
+    let viewModel: TipperViewModel
+
+    var body: some View {
+        HStack(alignment: .center) {
+            Text("Tip Amount")
+                .primaryStyle()
+            Spacer(minLength: 100)
+            Text(viewModel.getTipAmountStringFormatted(tipPercent: selectedTipPercentage, billAmount: billAmount))
+                .font(.subheadline)
+                .fontWeight(.regular)
+            // Color in hex #323A56
+                .foregroundColor(Color(red: 0.19607843137254902, green: 0.22745098039215686, blue: 0.33725490196078434))
+            Spacer()
+                .frame(width: 20)
+        }
     }
 }
